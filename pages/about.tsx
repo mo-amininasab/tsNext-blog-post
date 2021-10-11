7e68723fba7dd89ac132dcb7a149bd9f383d9687
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-11 21:29:36 +0330

    ⚡_document add

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,8 +9,8 @@ const AboutPage: NextPage<Props> = () => {
       title="About DevSpace"
       description="This is built with Next.js and Markdown"
     >
-      <h1 className="text-5xl border-b-4 pb-5 font-bold">About</h1>
-      <div className="bg-white shadow-md rounded-lg p-10 py-6 mt-6">
+      <h1 className="text-5xl font-semibold">About</h1>
+      <div className="bg-gray-100 shadow-lg rounded-lg p-10 py-6 mt-6">
         <h3 className="text-2xl mb-5">DevSpace Blog</h3>
         <p className="mb-3">This is built with Next.js and Markdown.</p>
         <p>
