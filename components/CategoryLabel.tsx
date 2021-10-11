140c9f3194067056e61cc8e9c58e9bd5621bcbac
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-12 01:43:43 +0330

    😪 trying another library for parsing MD

--- components/CategoryLabel.tsx
+++ components/CategoryLabel.tsx
@@ -13,28 +13,29 @@ const enum colorKeys {
 
 interface Props {
   category: string;
+  width?: number;
 }
 
-const CategoryLabel: React.FC<Props> = ({ category }) => {
+const CategoryLabel: React.FC<Props> = ({ category, width = 8 }) => {
   let icon: JSX.Element | '';
   switch (category) {
     case 'JavaScript':
-      icon = <DiJavascript1 className="w-8 h-8" />;
+      icon = <DiJavascript1 className={`w-${width} h-${width}`} />;
       break;
     case 'CSS':
-      icon = <DiCss3 className="w-8 h-8" />;
+      icon = <DiCss3 className={`w-${width} h-${width}`} />;
 
       break;
     case 'Python':
-      icon = <DiPython className="w-8 h-8" />;
+      icon = <DiPython className={`w-${width} h-${width}`} />;
 
       break;
     case 'PHP':
-      icon = <DiPhp className="w-8 h-8" />;
+      icon = <DiPhp className={`w-${width} h-${width}`} />;
 
       break;
     case 'Ruby':
-      icon = <DiRuby className="w-8 h-8" />;
+      icon = <DiRuby className={`w-${width} h-${width}`} />;
 
       break;
 
--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import marked from 'marked';
 
-import { GetStaticProps, NextPage, GetStaticPaths } from 'next'
-import Link from 'next/link'
-import Image from 'next/image'
+import { GetStaticProps, NextPage, GetStaticPaths } from 'next';
+import Image from 'next/image';
 
 import Layout from '../../components/Layout';
 import CategoryLabel from '../../components/CategoryLabel';
@@ -12,21 +12,19 @@ import CategoryLabel from '../../components/CategoryLabel';
 interface Props {
   frontMatter: {
     [key: string]: any;
-};
+  };
   content: string;
 }
 
-const PostDetailPage: NextPage<Props> = ({frontMatter, content}) => {
-
+const PostDetailPage: NextPage<Props> = ({ frontMatter, content }) => {
   return (
-<Layout title={frontMatter.title}>
-      <Link href="/blog">Go Back</Link>
+    <Layout title={frontMatter.title}>
       <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
         <div className="flex justify-between items-center mt-4">
           <h1 className="text-5xl mb-7">{frontMatter.title}</h1>
-          <CategoryLabel category={frontMatter.category}/>
+          <CategoryLabel category={frontMatter.category} width={12}/>
         </div>
-        <img src={frontMatter.cover_image} alt="" className="w-full rounded" />
+        <Image src={frontMatter.cover_image} alt={frontMatter.title} className="w-full rounded" width={688} height={500}/>
 
         <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
           <div className="flex items-center">
@@ -41,41 +39,43 @@ const PostDetailPage: NextPage<Props> = ({frontMatter, content}) => {
         </div>
 
         <div className="blog-text mt-2">
-          {/* <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div> */}
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
         </div>
       </div>
-    </Layout>  )
-}
+    </Layout>
+  );
+};
 
 export const getStaticPaths: GetStaticPaths = async (context) => {
   const filenames = fs.readdirSync(path.join('posts'));
-  const paths = filenames.map(filename => {
+  const paths = filenames.map((filename) => {
     const slug = filename.replace('.md', '');
 
     return {
-      params: {slug: slug}
-    }
-  })
-  
+      params: { slug: slug },
+    };
+  });
+
   return {
     paths: paths,
-    fallback: false
-  }
-}
+    fallback: false,
+  };
+};
 
-
-export const getStaticProps: GetStaticProps<Props> = async ({params}) => {
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   // @ts-ignore
-  const MDWithMeta = fs.readFileSync(path.join('posts', params.slug + '.md'), 'utf-8');
-  const {data: frontMatter, content} = matter(MDWithMeta);
-
+  const MDWithMeta = fs.readFileSync(
+    path.join('posts', params!.slug + '.md'),
+    'utf-8'
+  );
+  const { data: frontMatter, content } = matter(MDWithMeta);
 
   return {
     props: {
       frontMatter,
-      content
-    }
-  }
-}
+      content,
+    },
+  };
+};
 
-export default PostDetailPage
+export default PostDetailPage;
